d79994b34d07adf8ba300dc4c832cf72d1ee3e58
zhtsu <zhtsu@qq.com>
2023-07-19 03:44:36 +0800

    Complete flashlight shader & Add folder examples

---
+++ flashlight/ColorRect.gd
@@ -0,0 +1,15 @@
+extends ColorRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		material.set_shader_parameter("mouse_pos", event.position);
