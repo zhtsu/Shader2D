1b1d63c8bdde4c0e557f29db3f4f7a2eb57bc7a1
zhtsu <zhtsu@qq.com>
2023-09-26 23:12:27 +0800

    Add shader bar_transition

--- README.md
+++ README.md
@@ -13,5 +13,5 @@
 | NAME   | SCREENSHOT                       | NAME          | SCREENSHOT                              | NAME | SCREENSHOT                    | NAME | SCREENSHOT                     |
 |:------:| -------------------------------- |:------:|:---------------------------------------:|:----:|:-----------------------------:|:----:|:------------------------------:|
 | wave   | ![wave](.screenshots/wave.png)   | show   | ![wave](.screenshots/show.png)          | CRT  | ![wave](.screenshots/CRT.png) | gray | ![wave](.screenshots/gray.png) |
-| spiral | ![wave](.screenshots/spiral.png) | ripple | ![wave](.screenshots/water_surface.png) | knit | ![knit](.screenshots/knit.png) |      |                                |
+| spiral | ![wave](.screenshots/spiral.png) | ripple | ![wave](.screenshots/ripple.png) | knit | ![knit](.screenshots/knit.png) | bar transition | ![knit](.screenshots/bar_transition.png) |
 |        |                                  |        |                                         |      |                               |      |                                |
--- app/main.gd
+++ app/main.gd
@@ -1,20 +1,21 @@
 extends Control
 
 # Popup
-const popup_scene_res = preload("res://app/popup.tscn")
-const slot_scene_res = preload("res://app/slot.tscn")
+const tscn_popup = preload("res://app/popup.tscn")
+const tscn_slot = preload("res://app/slot.tscn")
 # Scenes for display shader
-const wave_scene_res = preload("res://shaders/wave/wave.tscn")
-const show_scene_res = preload("res://shaders/show/show.tscn")
-const spiral_scene_res = preload("res://shaders/spiral/spiral.tscn")
-const CRT_scene_res = preload("res://shaders/CRT/CRT.tscn")
-const gray_scene_res = preload("res://shaders/gray/gray.tscn")
-const ripple_scene_res = preload("res://shaders/ripple/ripple.tscn")
-const knit_scene_res = preload("res://shaders/knit/knit.tscn")
+const tscn_wave = preload("res://shaders/wave/wave.tscn")
+const tscn_show = preload("res://shaders/show/show.tscn")
+const tscn_spiral = preload("res://shaders/spiral/spiral.tscn")
+const tscn_crt = preload("res://shaders/crt/crt.tscn")
+const tscn_gray = preload("res://shaders/gray/gray.tscn")
+const tscn_ripple = preload("res://shaders/ripple/ripple.tscn")
+const tscn_knit = preload("res://shaders/knit/knit.tscn")
+const tscn_bar_transition = preload("res://shaders/bar_transition/bar_transition.tscn")
 
 
 func _create_slot(res : Resource, title : String) -> Node:
-	var slot = slot_scene_res.instantiate()
+	var slot = tscn_slot.instantiate()
 	slot.init_slot_data(res.instantiate(), title)
 	slot.connect("clicked", on_slot_clicked.bind(res.instantiate(), title))
 	return slot
@@ -26,19 +27,21 @@ func _ready() -> void:
 	
 	var slot_list = []
 	# Wave
-	slot_list.append(_create_slot(wave_scene_res, "Wave"))
+	slot_list.append(_create_slot(tscn_wave, "Wave"))
 	# Show
-	slot_list.append(_create_slot(show_scene_res, "Show"))
+	slot_list.append(_create_slot(tscn_show, "Show"))
 	# Spiral
-	slot_list.append(_create_slot(spiral_scene_res, "Spiral"))
+	slot_list.append(_create_slot(tscn_spiral, "Spiral"))
 	# CRT
-	slot_list.append(_create_slot(CRT_scene_res, "CRT"))
+	slot_list.append(_create_slot(tscn_crt, "CRT"))
 	# Gray
-	slot_list.append(_create_slot(gray_scene_res, "Gray"))
+	slot_list.append(_create_slot(tscn_gray, "Gray"))
 	# Ripple
-	slot_list.append(_create_slot(ripple_scene_res, "Ripple"))
+	slot_list.append(_create_slot(tscn_ripple, "Ripple"))
 	# Knit
-	slot_list.append(_create_slot(knit_scene_res, "Knit"))
+	slot_list.append(_create_slot(tscn_knit, "Knit"))
+	# Bar Transition
+	slot_list.append(_create_slot(tscn_bar_transition, "Bar\nTransition"))
 	# Add to Grid from list
 	for slot in slot_list:
 		$VBoxContainer/ContentBox/LeftBox/GridBox/Grid.add_child(slot)
@@ -61,7 +64,7 @@ func _link_to_github_repository() -> void:
 	OS.shell_open("https://github.com/zhtsu/Shader2D/")
 
 func _on_github_button_pressed() -> void:
-	var popup_scene = popup_scene_res.instantiate()
+	var popup_scene = tscn_popup.instantiate()
 	popup_scene.init_popup("Link to github repository ?", _link_to_github_repository)
 	add_child(popup_scene)
 	
@@ -70,7 +73,7 @@ func _link_to_qq_group_QR_code() -> void:
 	OS.shell_open("http://file.zhtsu.cc/qq_group_1065732551_QR_code.jpg")
 
 func _on_group_button_pressed() -> void:
-	var popup_scene = popup_scene_res.instantiate()
+	var popup_scene = tscn_popup.instantiate()
 	popup_scene.init_popup("Do you want to join\nthe QQ group ?", _link_to_qq_group_QR_code)
 	add_child(popup_scene)
 	
